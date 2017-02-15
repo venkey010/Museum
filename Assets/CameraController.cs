cc826d4cefcb32e84fee2938a5e23310b4f8c59e
venkey010 <venkey2venkatesh@gmail.com>
2017-02-16 02:07:37 +0530

    my first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    CharacterController c;
+	
+	void Start () {
+        c = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update () {
+	        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            c.SimpleMove(Camera.main.transform.forward * 5);
+        }
+            if (Input.GetKey(KeyCode.DownArrow))
+        {
+            c.SimpleMove(-Camera.main.transform.forward * 5);
+        }
+	}
+}
